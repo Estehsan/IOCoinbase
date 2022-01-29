d9ed38e40eefd7a95ca10093e5a479defae8edad
estehsan <games.huseb@email.com>
2022-01-29 18:35:30 +0500

    First Commit

---
+++ pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = ({ address }) => {
+  return (
+    <div>
+      <h1 className="text-white">{address}</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
